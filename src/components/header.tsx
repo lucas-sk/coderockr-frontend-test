8b4e52f99792a259df100044280661deb1555c73
lusk <lucas78654321@outlook.com>
2024-02-26 17:25:58 -0300

    styles: added height textarea and disable resize

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -55,7 +55,7 @@ export function Header() {
                     <textarea
                       placeholder="hello"
                       id="post"
-                      className="placeholder:text-black/50 border rounded-sm border-shaft-950 p-2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-shaft-950/50"
+                      className="placeholder:text-black/50 border h-[200px] rounded-sm border-shaft-950 p-2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-shaft-950/50 resize-none"
                     />
                   </label>
                   <Dialog.Close asChild>
