1ab9101f2243fcb379b7653260e1686b3500bff0
lusk <lucas78654321@outlook.com>
2024-02-26 17:26:31 -0300

    styles: added spaces and padding in defaultLayout

--- src/layout/defaultLayout.tsx
+++ src/layout/defaultLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from '../components/header'
 
 export function DefaultLayout() {
   return (
-    <div className="max-w-screen min-h-screen">
+    <div className="max-w-screen min-h-screen space-y-24 pb-28">
       <Header />
       <Outlet />
     </div>
