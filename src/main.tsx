180c5b108b2eaaa4749796fd65ffdd9eb160d1f7
lusk <lucas78654321@outlook.com>
2024-02-29 11:23:03 -0300

    refactor: created component App, remove content of main.tsx to app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { Toaster } from 'sonner'
 
-import { queryClient } from './lib/react-query'
-
-import './index.css'
-import { router } from './routes'
+import { App } from './app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Toaster />
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 )
